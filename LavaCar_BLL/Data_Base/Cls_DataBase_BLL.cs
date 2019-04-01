f6f897b6b86786075a88fc98e34a18461823432c
Josua22 <44511594+Josua22@users.noreply.github.com>
2019-03-31 23:34:52 -0600

    Creacion de la Conecxion a la base de datos SQLServer

---
+++ LavaCar_BLL/Data_Base/Cls_DataBase_BLL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LavaCar_DAL.Data_Base;
+
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LavaCar_BLL.Data_Base
+{
+    public class Cls_DataBase_BLL
+    {
+        //METODO DE CONECXION A LA BASE DE DATOS SQL SERVER
+
+        public void CrearConx(ref Cls_DataBase_DAL Obj_DB_DAL)
+        {
+            try
+            {
+                Obj_DB_DAL.sCxCadena = ConfigurationManager.ConnectionStrings["Windows_AUT"].ConnectionString;
+                Obj_DB_DAL.Obj_Connec_DT = new SqlConnection(Obj_DB_DAL.sCxCadena);
+                Obj_DB_DAL.sMsjError = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                Obj_DB_DAL.sMsjError = ex.Message.ToString();
+                Obj_DB_DAL.Obj_Connec_DT = null;
+                Obj_DB_DAL.sCxCadena = string.Empty; ;
+            }
+        }
+
+        //METODO DE CREACION DE PARAMETROS RELACIONANDO
+        //LAS VARIABLES DE LA BASE DE DATOS CON LA DE LA PLATAFORMA VISUAL
+
+        public void CrearParametros(ref Cls_DataBase_DAL Obj_DB_DAL)
+        {
+            try
+            {
+                Obj_DB_DAL.DT_Parametros = new DataTable("Parametros");
+                Obj_DB_DAL.DT_Parametros.Columns.Add("Nombre");
+                Obj_DB_DAL.DT_Parametros.Columns.Add("Tipo");
+                Obj_DB_DAL.DT_Parametros.Columns.Add("Valor");
+
+                Obj_DB_DAL.sMsjError = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                Obj_DB_DAL.sMsjError = ex.Message.ToString();
+                Obj_DB_DAL.DT_Parametros = null;
+            }
+        }
+    }
+}
---
+++ LavaCar_DAL/Data_Base/Cls_DataBase_DAL.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LavaCar_DAL.Data_Base
+{
+    public class Cls_DataBase_DAL
+    {
+        public DataTable DT_Parametros = new DataTable("Parametros");
+        public SqlConnection Obj_Connec_DT = new SqlConnection();
+        public SqlDataAdapter Obj_DAdapter = new SqlDataAdapter();
+        public DataSet Obj_DSet = new DataSet();
+        public SqlCommand Obj_Command;
+
+        private string _sCxCadena, _sTableName, _sSP_Name, _sScalarV, _sMsjError;
+
+        public string sCxCadena
+        {
+            get
+            {
+                return _sCxCadena;
+            }
+
+            set
+            {
+                _sCxCadena = value;
+            }
+        }
+
+        public string sTableName
+        {
+            get
+            {
+                return _sTableName;
+            }
+
+            set
+            {
+                _sTableName = value;
+            }
+        }
+
+        public string sScalarV
+        {
+            get
+            {
+                return _sScalarV;
+            }
+
+            set
+            {
+                _sScalarV = value;
+            }
+        }
+
+        public string sSP_Name
+        {
+            get
+            {
+                return _sSP_Name;
+            }
+
+            set
+            {
+                _sSP_Name = value;
+            }
+        }
+
+        public string sMsjError
+        {
+            get
+            {
+                return _sMsjError;
+            }
+
+            set
+            {
+                _sMsjError = value;
+            }
+        }
+    }
+}
