ecda2e88672a1350c884a89a0aed81acaebaec48
Kevin <Kevin@KEVIN>
2019-04-03 17:13:21 -0600

    BLL

--- LavaCar_BLL/Cat_Mant/cls_TransaccionesCompra_BLL.cs
+++ LavaCar_BLL/Cat_Mant/cls_TransaccionesCompra_BLL.cs
@@ -31,5 +31,29 @@ namespace LavaCar_BLL.Cat_Mant
                 return null;
             }
         }
+
+        public DataTable FiltrarTransaccionesCompra(ref string sMsgError, string sFiltro)
+        {
+            Cls_DataBase_DAL ObjDAL = new Cls_DataBase_DAL();
+            Cls_DataBase_BLL ObjBLL = new Cls_DataBase_BLL();
+
+            ObjBLL.CrearParametros(ref ObjDAL);
+            ObjDAL.DT_Parametros.Rows.Add("@Orden", 6, sFiltro);
+
+            ObjDAL.sTableName = "Transacciones Compra";
+            ObjDAL.sSP_Name = ConfigurationManager.AppSettings["Filtrar_TransaccionesCompra"].ToString().Trim();
+            ObjBLL.Execute_DataAdapter(ref ObjDAL);
+
+            if (ObjDAL.sMsjError == string.Empty)
+            {
+                sMsgError = string.Empty;
+                return ObjDAL.Obj_DSet.Tables[0];
+            }
+            else
+            {
+                sMsgError = ObjDAL.sMsjError;
+                return null;
+            }
+        }
     }
 }
