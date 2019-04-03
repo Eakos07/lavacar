3f13c0251bb9969abfb8058a1f362c6ea63bf35d
Kevin <Kevin@KEVIN>
2019-04-03 17:11:34 -0600

    BLL

--- LavaCar_BLL/Cat_Mant/cls_TransaccionesCompra_BLL.cs
+++ LavaCar_BLL/Cat_Mant/cls_TransaccionesCompra_BLL.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Configuration;
 using LavaCar_DAL.Data_Base;
-using LavaCar_DAL.Cat_Mant;
 using LavaCar_BLL.Data_Base;
 
 namespace LavaCar_BLL.Cat_Mant
@@ -19,30 +18,6 @@ namespace LavaCar_BLL.Cat_Mant
             Cls_DataBase_BLL ObjBLL = new Cls_DataBase_BLL();
             ObjDAL.sTableName = "Transacciones Compra";
             ObjDAL.sSP_Name = ConfigurationManager.AppSettings["Listar_TransaccionesCompra"].ToString().Trim();
-            ObjBLL.Execute_DataAdapter(ref ObjDAL);
-
-            if (ObjDAL.sMsjError == string.Empty)
-            {
-                sMsgError = string.Empty;
-                return ObjDAL.Obj_DSet.Tables[0];
-            }
-            else
-            {
-                sMsgError = ObjDAL.sMsjError;
-                return null;
-            }
-        }
-
-        public DataTable FiltrarTransaccionesCompra(ref string sMsgError, string sFiltro)
-        {
-            Cls_DataBase_DAL ObjDAL = new Cls_DataBase_DAL();
-            Cls_DataBase_BLL ObjBLL = new Cls_DataBase_BLL();
-
-            ObjBLL.CrearParametros(ref ObjDAL);
-            ObjDAL.DT_Parametros.Rows.Add("@Orden", 6, sFiltro);
-
-            ObjDAL.sTableName = "Transacciones Compra";
-            ObjDAL.sSP_Name = ConfigurationManager.AppSettings["Filtrar_TransaccionesCompra"].ToString().Trim();
             ObjBLL.Execute_DataAdapter(ref ObjDAL);
 
             if (ObjDAL.sMsjError == string.Empty)
