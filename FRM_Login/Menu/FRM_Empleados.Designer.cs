64648cd8bbb51d8b80577c24d9bd76aae2a8b57a
Josua22 <44511594+Josua22@users.noreply.github.com>
2019-04-05 13:20:18 -0600

    f

--- FRM_Login/Menu/FRM_Empleados.Designer.cs
+++ FRM_Login/Menu/FRM_Empleados.Designer.cs
@@ -138,7 +138,6 @@
             this.btn_Save.TabIndex = 11;
             this.btn_Save.Text = "Guardar";
             this.btn_Save.UseVisualStyleBackColor = false;
-            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
             // 
             // txt_Puesto
             // 
